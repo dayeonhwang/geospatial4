9d395b6544e4fd7d2e9f956223beabc3f5ea8216
Allen Tang <allentang2018@u.northwestern.edu>
2018-06-04 18:27:20 -0500

    added Xcode project

---
+++ pointcloud_registration/pointcloud_registration/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pointcloud_registration
+//
+//  Created by Allen Tang on 6/4/18.
+//  Copyright © 2018 Allen Tang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
